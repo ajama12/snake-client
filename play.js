72fe632c9e7380b94d09c1060346b6d1cefb55d3
Asma Jama <asmajama98@gmail.com>
2023-04-03 00:09:12 -0400

    added stdin

--- play.js
+++ play.js
@@ -1,4 +1,18 @@
 const { connect } = require("./client");
 console.log("Connecting ...");
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
 
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
